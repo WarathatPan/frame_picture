295870aaa70e70a3f37057d7f30c658fc4c7ebde
pan_w1ra <panthongthaworn_w@silpakorn.edu>
2019-05-29 09:56:54 +0700

    Frame_Picture

---
+++ Assets/Arduino_Initial.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+
+public class Arduino_Initial : MonoBehaviour
+{
+    public static SerialPort sp = new SerialPort("COM3", 57600);
+    public static int rot;
+    public static float rotX, rotY, rotZ;
+    // Use this for initialization
+    void Start()
+    {
+        OpenConnection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print(UnityReadData());
+        try
+        {
+            string[] s = sp.ReadLine().Split(',');
+
+            rotX = float.Parse(s[0]);
+            rotY = float.Parse(s[1]);
+            rotZ = float.Parse(s[2]);
+
+        }
+        catch (System.Exception) { }
+
+    }
+
+    public void OpenConnection()
+    {
+        if (sp != null)
+        {
+            if (sp.IsOpen)
+            {
+                sp.Close();
+                print("Closing port, because it was already open!");
+            }
+            else
+            {
+                sp.Open(); // opens the connection
+                sp.ReadTimeout = 100; //sets the timeout value before reporting error
+                print("Port Opend!");
+            }
+        }
+        else
+        {
+            if (sp.IsOpen)
+                print("Port is already open");
+            else
+                print("Port == null");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        sp.Close();
+    }
+
+    public static void UnitySendData(string m)
+    {
+        sp.Write(m);
+    }
+
+    /* public static string UnityReadData()
+     {
+         string me;
+         try
+         {
+             return(sp.ReadLine());
+         }catch(System.Exception){ return "0"; }
+
+         //return me;
+     }*/
+
+}
+
---
+++ Assets/Gravity_Change.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity_Change : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 mypos;
+    Collider2D mycolider;
+    Rigidbody2D myrigi;
+    Quaternion ROT;
+    float check = 0;
+    float start;
+    float count = 0;
+    float rtx, rty, rtz;
+	void Start () {
+        mypos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        ROT = transform.rotation;
+        mycolider = GetComponent<Collider2D>();
+        myrigi = GetComponent<Rigidbody2D>();
+        mycolider.enabled = false;
+        myrigi.simulated = false;
+
+       if (start == 0)
+           InvokeRepeating("startROT", 1f ,0.1f);
+            // Invoke("startROT", 1f);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // print("check : "+check);
+        print("RZ : "+Arduino_Initial.rotY);
+        // print(" count "+count);
+        if (start == 1)
+       {
+            mycolider.enabled = true;
+            myrigi.simulated = true;
+            float x = Mathf.Sin(Mathf.Deg2Rad * Arduino_Initial.rotY);
+            float y = Mathf.Cos(Mathf.Deg2Rad * Arduino_Initial.rotY);
+            Physics2D.gravity = new Vector2(-x, -y);
+            check = 0;
+        }
+
+        if (Arduino_Initial.rotY > -10f && Arduino_Initial.rotY < 10f)
+        {
+            check = 1;
+            count++;
+            Invoke("coutdown", 0f);
+        }
+        else {
+            check = 0;
+            count = 0;
+        }
+            
+        
+	}
+
+    void startROT()
+    {
+        if (start==0 )
+        {
+            if (Arduino_Initial.rotY > -10f && Arduino_Initial.rotY < 10f)
+            {
+                start = 0;
+            }
+            else {
+                start = 1;
+            }
+        }
+       
+    }
+
+    void coutdown()
+    {
+        if (check == 1 && count == 1)
+        {
+            Invoke("coutdown2", 1f);
+        }
+    }
+
+    void coutdown2()
+    {
+        if (check == 1 )
+        {
+            Invoke("coutdown3", 1f);
+        }
+    }
+
+    void coutdown3()
+    {
+        if (check == 1 )
+        {
+            Invoke("coutdown4", 1f);
+        }
+    }
+
+    void coutdown4()
+    {
+        if (check == 1 )
+        {
+            Invoke("backtobegin", 0f);
+        }
+    }
+    void backtobegin()
+    {
+        mycolider.enabled = false;
+        myrigi.simulated = false;
+        transform.position = mypos;
+        transform.rotation = ROT;
+        Invoke("startROT", 1f);
+        count = 0;
+        start = 0;
+    }
+
+}
